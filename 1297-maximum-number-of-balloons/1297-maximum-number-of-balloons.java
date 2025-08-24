1bcabde58e782717cb572b51ab8576fd82d3fb84
Anthony Villegas <61989465+AnthonyVillegas87@users.noreply.github.com>
2025-08-24 10:07:15 -0500

    Time: 10 ms (36.92%), Space: 42.9 MB (11.32%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -1,31 +1,21 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-      Map<Character, Integer> textCounts = new HashMap<>();
-
-      for(char c : text.toCharArray()) {
-        textCounts.put(c, textCounts.getOrDefault(c, 0) + 1);
-      }
-
-      Map<Character, Integer> balloonCounts = new HashMap<>();
-      balloonCounts.put('b', 1);
-      balloonCounts.put('a', 1);
-      balloonCounts.put('l', 2);
-      balloonCounts.put('o', 2);
-      balloonCounts.put('n', 1);
-
-       int minCount = Integer.MAX_VALUE;
-
-       for(Map.Entry<Character, Integer> entry : balloonCounts.entrySet()) {
-        char character = entry.getKey();
-        int balloonFrequency = entry.getValue();
-
-        int textFrequency = textCounts.getOrDefault(character, 0);
-
-        int possibleBalloons = textFrequency / balloonFrequency;
-
-        minCount = Math.min(minCount, possibleBalloons);
-       }
-
-       return minCount;
+        // Create a hashmap to store character frequencies
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        
+        // Populate the hashmap with character frequencies from the string
+        for (char c : text.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        
+        int minCount = Integer.MAX_VALUE;
+        // Calculate the maximum number of times "balloon" can be formed
+        minCount = Math.min(minCount, charCount.getOrDefault('b', 0));
+        minCount = Math.min(minCount, charCount.getOrDefault('a', 0));
+        minCount = Math.min(minCount, charCount.getOrDefault('l', 0) / 2);
+        minCount = Math.min(minCount, charCount.getOrDefault('o', 0) / 2);
+        minCount = Math.min(minCount, charCount.getOrDefault('n', 0));
+        
+        return minCount;
     }
 }
